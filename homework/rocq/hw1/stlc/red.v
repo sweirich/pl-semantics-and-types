0a862f4b2241e9dabcec559e35ae7f953c5e5c6d
Stephanie Weirich <sweirich@seas.upenn.edu>
2025-09-06 15:19:39 -0400

    homework rocq files

---
+++ homework/rocq/hw1/stlc/red.v
@@ -0,0 +1,185 @@
+From Stdlib Require Export ssreflect.
+From Stdlib Require Export Program.Equality.
+Require Export common.fintype.
+Require Export common.relations.
+Require Export stlc.syntax.
+
+(* This file defines multiple reduction relations for 
+   STLC + nat. 
+*)
+  
+Import ScopedNotations.
+
+Definition is_value {n} (e : Tm n) : bool := 
+  match e with 
+  | var _ => true  (* is this ok? why or why not? *)
+  | abs _ => true
+  | lit k => true
+  | _ => false
+  end.
+
+(** Small step, substitution-based reduction relation *)
+
+Module Small. 
+
+Inductive step : Tm 0 -> Tm 0 -> Prop := 
+
+ | s_beta e v : 
+    is_value v = true ->
+    step (app (abs e) v) (e [v..])
+
+ | s_app_cong1 e1 e1' e2 : 
+    step e1 e1' ->
+    step (app e1 e2) (app e1' e2)
+
+ | s_app_cong2 v e2 e2' : 
+    is_value v = true ->
+    step e2 e2' ->
+    step (app v e2) (app v e2')
+ | s_succ_lit k : 
+    step (succ (lit k)) (lit (S k))
+  
+ | s_succ_cong e e' :
+   step e e' -> 
+   step (succ e) (succ e')
+
+ | s_nrec_zero e0 e1 : 
+    step (nrec (lit 0) e0 e1) e0
+
+ | s_nrec_succ k e0 e1 : 
+    step (nrec (lit (S k)) e0 e1) (app (e1 [(lit k) ..]) (nrec (lit k) e0 e1))
+
+ | s_nrec_cong e e' e0 e1 :
+    step e e' ->
+    step (nrec e e0 e1) (nrec e' e0 e1)
+
+ | s_letv v e :
+    is_value v = true ->
+    step (let_ v e) (e [v ..])
+
+ | s_let_cong e1 e1' e2 :
+    step e1 e1' ->
+    step (let_ e1 e2) (let_ e1' e2)
+.
+
+End Small.
+
+(** Big step, substitution-based reduction relation *)
+
+Module Big.
+
+Inductive step : Tm 0 -> Tm 0 -> Prop := 
+  | s_val v : 
+    is_value v = true -> 
+    step v v
+
+  | s_app  e1 e1' e2 v1 v2 :
+    step e1 (abs e1') ->
+    step e2 v1 ->
+    step (e1' [v1 .. ]) v2 ->
+    step (app e1 e2) v2
+  | s_succ e k :
+    step e (lit k) ->
+    step (succ e) (lit (S k))
+
+  | s_nrec e e0 e1 k v : 
+    step e (lit k) -> 
+    step_nrec k e0 e1 v ->
+    step (nrec e e0 e1) v
+
+  | s_let e1 e2 v1 v2 :  
+    step e1 v1 -> 
+    step (e2 [v1 ..]) v2 ->
+    step (let_ e1 e2) v2
+with 
+  step_nrec : nat -> Tm 0 -> Tm 1 -> Tm 0 -> Prop :=
+  | s_nrec_zero v e0 e1 : 
+    step e0 v ->
+    step_nrec 0 e0 e1 v
+  | s_nrec_succ k e0 e1 v1 e' v :
+    step e1[(lit k) ..] (abs e') ->
+    step_nrec k e0 e1 v1 ->
+    step e'[v1 ..] v ->
+    step_nrec (S k) e0 e1 v
+.
+
+Scheme step_ind' := Induction for step Sort Prop
+with step_nrec_ind' := Induction for step_nrec Sort Prop.
+
+Combined Scheme step_step_nrec_mutind from step_ind',step_nrec_ind'.
+
+
+End Big.
+
+
+
+(** Big step, environment-based reduction relation *)
+
+Module BigEnv.
+
+Inductive Val : Type :=
+  | v_clo {n} : (fin n -> Val) -> Tm (S n) -> Val
+  | v_lit : nat -> Val.
+
+Definition Env n := fin n -> Val.
+
+Inductive step {n} (ρ : Env n) : Tm n -> Val -> Prop := 
+
+  | s_var x : 
+    step ρ (var x) (ρ x)
+
+  | s_abs e : 
+    step ρ (abs e) (v_clo ρ e)
+
+  | s_app e1 {m} (ρ' : Env m) e1' e2 v1 v2 :
+    step ρ e1 (v_clo ρ' e1') ->
+    step ρ e2 v1 ->
+    step (v1 .: ρ') e1' v2 ->
+    step ρ (app e1 e2) v2
+
+  | s_lit k : 
+    step ρ (lit k) (v_lit k)
+
+  | s_succ e n : 
+    step ρ e (v_lit n) ->
+    step ρ (succ e) (v_lit (S n))
+
+  | s_nrec e e1 e2 n v : 
+    step ρ e (v_lit n) -> 
+    step_nrec ρ n e1 e2 v -> 
+    step ρ (nrec e e1 e2) v
+
+  | s_let e1 e2 v1 v2 : 
+    step ρ e1 v1 ->
+    step (v1 .: ρ) e2 v2 -> 
+    step ρ (let_ e1 e2) v2
+
+with 
+  step_nrec {n} (ρ : Env n) : nat -> Tm n -> Tm (S n) -> Val -> Prop :=
+  | s_nrec_zero v e0 e1 : 
+    step ρ e0 v ->
+    step_nrec ρ 0 e0 e1 v
+  | s_nrec_succ n e0 e1 v1 {m} (ρ': Env m) e' v :
+    step_nrec ρ n e0 e1 v1 ->
+    step (v_lit n .: ρ) e1 (v_clo ρ' e') ->
+    step (v1 .: ρ') e' v ->
+    step_nrec ρ (S n) e0 e1 v
+.
+
+Scheme step_ind' := Induction for step Sort Prop
+with step_nrec_ind' := Induction for step_nrec Sort Prop.
+
+Combined Scheme step_step_nrec_mutind from step_ind',step_nrec_ind'.
+
+End BigEnv.   
+
+Module RedNotations.  
+Infix "⤳"  := Small.step (at level 70).
+Infix "⤳*" := (multi Small.step) (at level 70).
+Notation "e ⟱ v" := 
+  (e ⤳* v /\ is_value (v : Tm 0) = true) (at level 70).
+Infix "⇓"   := Big.step   (at level 70).
+Notation "< ρ | e > ⇓ v" := (BigEnv.step ρ e v) (at level 70).
+End RedNotations.
+
+
